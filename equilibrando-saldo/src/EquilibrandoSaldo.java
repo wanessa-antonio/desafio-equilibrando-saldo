e2d8f327f66499b649a4714ad9f413596aa06185
Wanessa <wanessa.ant04@gmail.com>
2023-09-27 19:50:34 -0300

    commit add. 2

--- equilibrando-saldo/src/EquilibrandoSaldo.java
+++ equilibrando-saldo/src/EquilibrandoSaldo.java
@@ -1,20 +1,25 @@
 import java.util.Scanner;
-
 public class EquilibrandoSaldo {
-
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
             // Solicita ao usuário que insira o saldo atual
             System.out.print("Digite o saldo atual: ");
             double saldoAtual = scanner.nextDouble();
 
+            // Adiciona uma linha em branco
+            System.out.println();
+
             // Solicita ao usuário que insira o valor do depósito
             System.out.print("Digite o valor do depósito: ");
             double valorDeposito = scanner.nextDouble();
 
+            System.out.println();
+
             // Solicita ao usuário que insira o valor da retirada
             System.out.print("Digite o valor da retirada: ");
             double valorRetirada = scanner.nextDouble();
+
+            System.out.println();
 
             // Calcula o novo saldo após as transações
             double saldoAtualizado = saldoAtual + valorDeposito - valorRetirada;
